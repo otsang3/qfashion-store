705eeb870f6c28e5b21ab9fe6571773286a4392b
otsang3 <ohhtsang@gmail.com>
2020-09-20 18:24:31 +0100

    added createRouter

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+const createRouter = require('./helpers/createRouter.js')
+
+app.use(cors());
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('qfashionDatabase')
+  const storeCollection = db.collection('store')
+  const storeRouter = createRouter(storeCollection)
+  app.use('/api/films', storeRouter)
+})
+
+app.listen(3000, function(){
+  console.log(`This app is listening on ${this.address().port}`);
+})
