a61553a5543663079ef91699f855a1899e7c7693
otsang3 <ohhtsang@gmail.com>
2020-09-24 20:41:45 +0100

    added image preview functionality in product page

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -4,11 +4,12 @@ function Product(props) {
 
     const prodName = props.item.name.split(" ").join("-").toLowerCase();
     const prodUrl = `/${props.category}/${props.prodType}/${prodName}`
+    console.log(props.item.imgUrl[0]);
 
     return(
         <div className="prod-container">
             <a href={prodUrl}>
-            <img src={require("../images" + props.item.imgUrl)} alt="product"/>
+            <img src={require("../images" + props.item.imgUrl[0])} alt="product"/>
             <p>{props.item.name}</p>
             </a>
         </div>
--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ProductDetail(props) {
 
+    const [imgDisplay, setDisplay] = useState(props.item.imgUrl[0])
+
     const item = props.item;
+
+    const changeDisplay = (imgUrl) => {
+        setDisplay(imgUrl);
+    }
+
     const renderOptions = () => {
         const sizeArr = [];
         item.size.map((size, index) => sizeArr.push(<option key={index}>{size}</option>))
         return sizeArr;
     }
-    console.log(props);
+
+    const renderPreviews = () => {
+        const imageArr = []
+        item.imgUrl.map(image => {
+            imageArr.push(<img onClick={() => changeDisplay(image)} src={require('../images' + image)}/>)
+        })
+        return imageArr;
+    }
 
     return(
         <div className="prod-detail-container">
             <div className="prod-detail-row-1">
-                <img src={require("../images" + props.item.imgUrl)} alt="product"/>
+                {renderPreviews()}
             </div>
             <div className="prod-detail-row-2">
+                <img src={require("../images" + imgDisplay)} alt="product"/>
+            </div>
+            <div className="prod-detail-row-3">
                 <h4>{item.name}</h4>
                 <h4>£{item.price}</h4>
                 <p>Colour: {item.colour}</p>
